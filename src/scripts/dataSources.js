abc805c60593bc68badd7dfb13b9dcd771a0eca4
jdbbaugh <jbaugh78@msn.com>
2019-01-09 07:41:08 -0600

    made object that combines elements of different sources and console logging it

--- src/scripts/dataSources.js
+++ src/scripts/dataSources.js
@@ -1,8 +1,71 @@
 const dataSources = {
-  getEmployees() {
-    return fetch("http://localhost:8088/employees")
-    .then(response => response.json())
-  },
-}
+  employees: [
+      {
+        "emplyeeId": 1,
+        "employeeName": "Hernando Smith",
+        "departmentId": 1,
+        "computerId": 4
+      },
+      {
+        "emplyeeId": 2,
+        "employeeName": "Jenny Zimmerman",
+        "departmentId": 3,
+        "computerId": 2
+      },
+      {
+        "emplyeeId": 3,
+        "employeeName": "Bob Hope",
+        "departmentId": 2,
+        "computerId": 3
+      },
+      {
+        "emplyeeId": 4,
+        "employeeName": "Julia Gulia",
+        "departmentId": 5,
+        "computerId": 1
+      },
+    ],
+    
+    departments: [
+      {
+        "departmentId": 1,
+        "departmentName": "I.T. Department"
+      },
+      {
+        "departmentId": 2,
+        "departmentName": "Marketing"
+      },
+      {
+        "departmentId": 3,
+        "departmentName": "BlackMailing"
+      },
+      {
+        "departmentId": 4,
+        "departmentName": "Hitman Department"
+      },
+      {
+        "departmentId": 5,
+        "departmentName": "Customer Service"
+      },
+    ],
+    
+    computers: [
+      {
+        "computerId": 1,
+        "computerType": "Apple MacBook Pro"
+      },
+      {
+        "computerId": 2,
+        "computerType": "Mcintosh Surface Pro"
+      },
+      {
+        "computerId": 3,
+        "computerType": "Atari 2600"
+      },
+      {
+        "computerId": 4,
+        "computerType": "Randy's calculator"
+      },
+    ]}
 
-export default dataSources
+    export default dataSources
